023ebd14028a52d38afcbf012225ecf790e70cba
Abdulkadir <37668783+Abdulkadir31@users.noreply.github.com>
2019-01-04 12:54:23 +0530

    Line Sepearation

--- imagepr.py
+++ imagepr.py
@@ -59,16 +59,61 @@ while i < row:
         i = i + 1
 
 # Display the images of lines
+# i=0
+# while i < len(img_cor):
+#     line = th3[img_cor[i]:img_cor[i+1],0:col]
+#     cv2.imshow(frame,line)
+#     i=i+2
+#     frame=frame+str(i)
+#     cv2.waitKey(0)
+#     cv2.destroyAllWindows()
+
+# Word Seperation
+
+line = th3[img_cor[0]:img_cor[1],0:col]
+cv2.imshow("frame",line)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
+word_row=line.shape[0]
+word_col=line.shape[1]
+
+
 i=0
-while i < len(img_cor):
-    line = th3[img_cor[i]:img_cor[i+1],0:col]
-    cv2.imshow(frame,line)
+count = 0
+word_pixelval=list()
+for i in range(word_col+1):
+    word_pixelval.append(0)
+
+# Logic
+
+for i in range(1,word_col):
+    count = 0
+    for j in range(1,word_row):
+       if line[j,i]==0:
+           count=count+1
+    word_pixelval[i]=count
+
+i=0
+th=0
+word_cor = list()
+while i < word_col:
+    if word_pixelval[i]>th:
+        start=i
+        end=i
+        word_cor.append(start)
+        while word_pixelval[end] > th and end < word_col:
+            end=end+1
+        i=end+1
+        word_cor.append(i)
+    else:
+        i=i+1
+print(word_cor)
+i=0
+while i < len(word_cor):
+    line1 = line[0:word_row,word_cor[i]:word_cor[i+1]]
+    cv2.imshow(frame,line1)
     i=i+2
     frame=frame+str(i)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
-
-# Word Seperation
-
-
-
